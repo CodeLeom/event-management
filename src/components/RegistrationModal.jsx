2e8b59d809de13e68f702357d79f41e87648084d
Aransiola Ayodele <36636824+CodeLeom@users.noreply.github.com>
2025-01-21 13:39:10 +0100

    remove other from gender

--- src/components/RegistrationModal.jsx
+++ src/components/RegistrationModal.jsx
@@ -184,7 +184,6 @@ export default function RegistrationModal({ onClose }) {
             >
               <option value="Male">Male</option>
               <option value="Female">Female</option>
-              <option value="Other">Other</option>
             </select>
           </div>
 
